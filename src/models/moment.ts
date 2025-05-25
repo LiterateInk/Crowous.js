b4f885b3935dad2d7c6f0c1d803cdf548ffb7d75
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-05-25 22:31:39 +0200

    feat: add `toOpeningDayIndex` function to `Moment` namespace

--- src/models/moment.ts
+++ src/models/moment.ts
@@ -3,3 +3,22 @@ export enum Moment {
   LUNCH = "LUNCH",
   MORNING = "MORNING"
 }
+
+// eslint-disable-next-line ts/no-namespace
+export namespace Moment {
+  /**
+   * An opening day string looks like this: `001`, for example.
+   * There's three numbers because it is actually "morning, lunch, evening" values.
+   * `0` means closed, `1` means opened.
+   *
+   * This function retrieves the proper index depending on a given `Moment` value.
+   */
+  export function toOpeningDayIndex(moment: Moment): 0 | 1 | 2 {
+    switch (moment) {
+      case Moment.EVENING: return 2;
+      case Moment.LUNCH: return 1;
+      case Moment.MORNING: return 0;
+      default: throw new Error();
+    }
+  }
+}
